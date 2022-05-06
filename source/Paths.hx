1b9689ca4643fdc3f292e2294689bfac6368d598
Paidyy <wieszielonagora2@gmail.com>
2022-05-07 00:19:50 +0200

    update path stuff

--- source/Paths.hx
+++ source/Paths.hx
@@ -1,5 +1,6 @@
 package;
 
+import Song.SwagSong;
 import sys.io.File;
 import sys.FileSystem;
 import flixel.FlxG;
@@ -20,11 +21,17 @@ using StringTools;
 class Paths {
 
 	// improve these because it's shit
-	inline public static var SOUND_EXT = #if web "mp3" #else "ogg" #end;
 
-	static var currentLevel:String = "week0";
+	inline public static var FS = #if neko '/' #else '\\' #end;
 
-	static var currentStage:String = "stage";
+	inline public static var SOUND_EXT = #if web 'mp3' #else 'ogg' #end;
+
+	//for android support in future?
+	public static var modsLoc:String = "mods";
+
+	static var currentLevel:String = 'week0';
+
+	static var currentStage:String = 'stage';
 
 	static public function setCurrentLevel(name:String) {
 		currentLevel = name.toLowerCase();
@@ -33,6 +40,12 @@ class Paths {
 	static public function setCurrentStage(stage:String) {
 		currentStage = stage;
 	}
+
+	public static function slashPath(path:String) {
+		path = path.replace("/", FS);
+		path = path.replace("\\", FS);
+		return path;
+	} 
 
 	public static function isCustomPath(arg0:String) {
 		if (OpenFlAssets.exists(arg0)) {
@@ -48,14 +61,14 @@ class Paths {
 		if (currentLevel != null) {
 			var levelPath = getLibraryPathForce(file, currentLevel);
 			/*
-			if (!(levelPath.contains("NOTE_assets") || levelPath.contains("alphabet"))) {
-				trace(levelPath + " | " + OpenFlAssets.exists(levelPath, type));
+			if (!(levelPath.contains('NOTE_assets') || levelPath.contains('alphabet'))) {
+				trace(levelPath + ' | ' + OpenFlAssets.exists(levelPath, type));
 			}
 			*/
 			if (OpenFlAssets.exists(levelPath, type))
 				return levelPath;
 
-			levelPath = getLibraryPathForce(file, "shared");
+			levelPath = getLibraryPathForce(file, 'shared');
 			if (OpenFlAssets.exists(levelPath, type))
 				return levelPath;
 		}
@@ -63,28 +76,99 @@ class Paths {
 		return getPreloadPath(file);
 	}
 
+	public static function getSongGlobalNotesPath(songName:String) {
+		return getSongPath(songName, true) + 'global_notes.json';
+	}
+
+	public static function getSongPath(songName:String, ?dataFolder = false) {
+		if (FileSystem.exists('${modsLoc}${FS}songs${FS}' + songName.toLowerCase() + '${FS}')) {
+			return '${modsLoc}${FS}songs${FS}' + songName.toLowerCase() + ${FS};
+		}
+		else if (FileSystem.exists('assets${FS}' + (dataFolder ? 'data' : 'songs') + ${FS} + songName.toLowerCase() + ${FS})) {
+			return 'assets${FS}' + (dataFolder ? 'data' : 'songs') + ${FS} + songName.toLowerCase() + ${FS};
+		}
+		return null;
+	}
+
+	public static function getStagePath(stageName:String) {
+		if (FileSystem.exists('${modsLoc}/stages/' + stageName.toLowerCase() + '/')) {
+			return '${modsLoc}/stages/' + stageName.toLowerCase() + '/';
+		}
+		else if (FileSystem.exists('assets/stages/' + stageName.toLowerCase() + '/')) {
+			return 'assets/stages/' + stageName.toLowerCase() + '/';
+		}
+		return null;
+	}
+
+	/**
+	 * WARNING: GETS THE SYS PATH, NOT OPENFL PATH
+	 */
+	public static function getCharacterPath(characterName:String) {
+		if (!characterName.endsWith('-custom')) {
+			if (FileSystem.exists('${modsLoc}/characters/' + characterName + '/')) {
+				return '${modsLoc}/characters/' + characterName + '/';
+			}
+			else if (FileSystem.exists('assets/shared/images/characters/' + characterName + '/')) {
+				return 'assets/shared/images/characters/' + characterName + '/';
+			}
+		}
+		else {
+			switch (characterName) {
+				case 'bf-custom':
+					return Options.customBfPath;
+				case 'gf-custom':
+					return Options.customGfPath;
+				case 'dad-custom':
+					return Options.customDadPath;
+			}
+		}
+		return null;
+	}
+
+	public static function getSongJson(songName:String, ?difficulty:Int):SwagSong {
+		var dataFileDifficulty:String = '';
+		switch (difficulty) {
+			case 0:
+				dataFileDifficulty = '-easy';
+			case 1:
+				dataFileDifficulty = '';
+			case 2:
+				dataFileDifficulty = '-hard';
+		}
+
+		var json:SwagSong;
+		if (FileSystem.exists(Paths.instNoLib(songName.toLowerCase()))) {
+			json = Song.loadFromJson(songName.toLowerCase() + dataFileDifficulty, songName.toLowerCase());
+		}
+		else {
+			json = Song.PEloadFromJson(songName.toLowerCase() + dataFileDifficulty, songName.toLowerCase());
+		}
+		return json;
+	}
+
+	@:deprecated('use getSongPath()')
 	public static function getSongFolder(songName:String):String {
 		var song = songName.toLowerCase();
 
-		for (file in FileSystem.readDirectory("mods/songs/")) {
-			var path = haxe.io.Path.join(["mods/songs/", file]);
+		for (file in FileSystem.readDirectory('${modsLoc}/songs/')) {
+			var path = haxe.io.Path.join(['${modsLoc}/songs/', file]);
 			if (FileSystem.isDirectory(path) && file == song) {
-				return path + "/";
+				return path + '/';
 			}
 		}
-		for (file in FileSystem.readDirectory("assets/songs/")) {
-			var path = haxe.io.Path.join(["assets/songs/", file]);
+		for (file in FileSystem.readDirectory('assets/songs/')) {
+			var path = haxe.io.Path.join(['assets/songs/', file]);
 			if (FileSystem.isDirectory(path)) {
 				if (FileSystem.isDirectory(path) && file == song) {
-					return path + "/";
+					return path + '/';
 				}
 			}
 		}
 		return null;
 	}
 
-	static public function getLibraryPath(file:String, library = "preload") {
-		return if (library == "preload" || library == "default") getPreloadPath(file); else getLibraryPathForce(file, library);
+	static public function getLibraryPath(file:String, library = 'preload') {
+		return if (library == 'preload' || library == 'default') getPreloadPath(file); else getLibraryPathForce(file, library);
 	}
 
 	inline static function getLibraryPathForce(file:String, library:String) {
@@ -117,22 +201,22 @@ class Paths {
 
 	static public function stageMusic(key:String, ?stage:String) {
 		if (stage == null) stage = currentStage;
-		return getLibraryPathForce('$stage/music/$key.$SOUND_EXT', "stages");
+		return getLibraryPathForce('$stage/music/$key.$SOUND_EXT', 'stages');
 	}
 
 	inline static public function stageImage(key:String, ?stage:String) {
 		if (stage == null) stage = currentStage;
-		return getLibraryPathForce('$stage/images/$key.png', "stages");
+		return getLibraryPathForce('$stage/images/$key.png', 'stages');
 	}
 
 	inline static public function stageSparrow(key:String, ?stage:String) {
 		if (stage == null) stage = currentStage;
-		return FlxAtlasFrames.fromSparrow(stageImage(key, stage), getLibraryPathForce('$stage/images/$key.xml', "stages"));
+		return FlxAtlasFrames.fromSparrow(stageImage(key, stage), getLibraryPathForce('$stage/images/$key.xml', 'stages'));
 	}
 
 	inline static public function stagePacker(key:String, ?stage:String) {
 		if (stage == null) stage = currentStage;
-		return FlxAtlasFrames.fromSpriteSheetPacker(stageImage(key, stage), getLibraryPathForce('$stage/images/$key.txt', "stages"));
+		return FlxAtlasFrames.fromSpriteSheetPacker(stageImage(key, stage), getLibraryPathForce('$stage/images/$key.txt', 'stages'));
 	}
 
 	inline static public function soundRandom(key:String, min:Int, max:Int, ?library:String) {
@@ -160,44 +244,44 @@ class Paths {
 	}
 
 	inline static public function PEvoices(song:String) {
-		return 'mods/songs/${song.toLowerCase()}/Voices.$SOUND_EXT';
+		return '${modsLoc}/songs/${song.toLowerCase()}/Voices.$SOUND_EXT';
 	}
 
 	inline static public function PEinst(song:String) {
-		return 'mods/songs/${song.toLowerCase()}/Inst.$SOUND_EXT';
+		return '${modsLoc}/songs/${song.toLowerCase()}/Inst.$SOUND_EXT';
 	}
 
 	inline static public function getLuaPath(song:String) {
-		return CoolUtil.getSongPath(song.toLowerCase(), true) + 'script.lua';
+		return getSongPath(song.toLowerCase(), true) + 'script.lua';
 	}
 
 	static public function skinIcon(char:String):String {
-		if (char == "bf") {
+		if (char == 'bf') {
 			return Options.customBfPath + 'icon.png';
 		}
-		else if (char == "dad") {
+		else if (char == 'dad') {
 			return Options.customDadPath + 'icon.png';
 		}
-		else if (char == "gf") {
+		else if (char == 'gf') {
 			return Options.customGfPath + 'icon.png';
 		}
 		return null;
 	}
 
 	inline static public function modsIcon(char:String) {
-		return CoolUtil.getCharacterPath(char) + 'icon.png';
+		return getCharacterPath(char) + 'icon.png';
 	}
 
 	inline static public function PEgetSparrowAtlas(key:String, ?library:String) {
 		#if sys
-		return FlxAtlasFrames.fromSparrow(BitmapData.fromBytes(File.getBytes(key + ".png")), File.getContent(key + ".xml"));
+		return FlxAtlasFrames.fromSparrow(BitmapData.fromBytes(File.getBytes(key + '.png')), File.getContent(key + '.xml'));
 		#else
 		return Paths.getSparrowAtlas('DADDY_DEAREST');
 		#end
 	}
 
 	inline static public function PEgetPackerAtlas(key:String) {
-		return FlxAtlasFrames.fromSpriteSheetPacker(BitmapData.fromBytes(File.getBytes(key + ".png")), File.getContent(key + ".txt"));
+		return FlxAtlasFrames.fromSpriteSheetPacker(BitmapData.fromBytes(File.getBytes(key + '.png')), File.getContent(key + '.txt'));
 	}
 
 	/*
@@ -213,15 +297,15 @@ class Paths {
 	*/
 	
 	static public function isPathCustom(path:String) {
-		return path.contains("mods/");
+		return path.contains('${modsLoc}/');
 	}
 
 	static public function portrait(char:String) {
 		if (OpenFlAssets.exists(image('portraits/${char}'), IMAGE)) {
 			return image('portraits/${char}');
 		}
-		else if (FileSystem.exists('mods/portraits/$char.png')) {
-			return 'mods/portraits/$char.png';
+		else if (FileSystem.exists('${modsLoc}/portraits/$char.png')) {
+			return '${modsLoc}/portraits/$char.png';
 		}
 		return null;
 	}
