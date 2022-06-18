1d23244b143c939b98f283a5dbc1d8caf00e9267
Paidyy <wieszielonagora2@gmail.com>
2022-06-19 00:08:51 +0200

    rewritten skin selector

--- source/OptionsSubState.hx
+++ source/OptionsSubState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.group.FlxSpriteGroup;
 import flixel.util.FlxTimer;
 import sys.FileSystem;
 import flixel.FlxObject;
@@ -140,12 +141,22 @@ class OptionsPrefencesSubState extends OptionSubState {
 			new OptionItem('BF Skin'),
 			new OptionItem('GF Skin'),
 			new OptionItem('Dad Skin')
+			#if debug
+			,new OptionItem('Customize Note')
+			#end
 		];
 		super(items, inGame);
 	}
 	
 	override public function update(elapsed) {
 		super.update(elapsed);
+
+		if (Controls.check(ACCEPT)) {
+			if (itemList[curSelected].text == "Customize Note") {
+				closeSubState();
+				FlxG.state.openSubState(new NoteOption(inGame));
+			}
+		}
 
 		if (Controls.check(UI_RIGHT) || Controls.check(UI_LEFT)) {
 			if (Controls.check(UI_LEFT)) {
@@ -192,13 +203,13 @@ class OptionsPrefencesSubState extends OptionSubState {
 			switch (itemList[curSelected].text) {
 				case "BF Skin":
 					closeSubState();
-					FlxG.state.openSubState(new OptionsCharacterSubState("bf", inGame));
+					FlxG.state.openSubState(new OptionsSkinSubState("bf", inGame));
 				case "GF Skin":
 					closeSubState();
-					FlxG.state.openSubState(new OptionsCharacterSubState("gf", inGame));
+					FlxG.state.openSubState(new OptionsSkinSubState("gf", inGame));
 				case "Dad Skin":
 					closeSubState();
-					FlxG.state.openSubState(new OptionsCharacterSubState("dad", inGame));	
+					FlxG.state.openSubState(new OptionsSkinSubState("dad", inGame));	
 			}
 		}
 	}
@@ -440,129 +451,166 @@ class Background extends FlxSprite {
 	}
 }
 
-class OptionsCharacterSubState extends FlxSubState {
-
-    var character:String;
-    var character_path:String;
-    var character_list:Array<String>;
-
-    var grpTexts:FlxTypedGroup<FlxText>;
-    var inGame = false;
+class OptionsSkinSubState extends FlxSubState {
+	var inGame = false;
 	
-    override public function new(character, ?inGame) {
-        super();
+	var skinPaths:Array<String> = [];
 
-        this.inGame = inGame;
-        this.character = character;
-		character_path = ${Paths.modsLoc} + "/skins/" + character + "/";
-        character_list = new Array<String>();
+	override public function new(character, ?inGame) {
+		super();
 
-        character_list.push("Vanilla");
-		for (file in FileSystem.readDirectory(character_path)) {
-			var path = haxe.io.Path.join([character_path, file]);
-            if (FileSystem.isDirectory(path)) {
-                character_list.push(file);
-            }
-		}
+		this.inGame = inGame;
+		this.character = character;
 
 		var bg = new Background(FlxColor.BLACK, true);
 		bg.alpha = 0.7;
 		add(bg);
 
-        grpTexts = new FlxTypedGroup<FlxText>();
+		grpTexts = new FlxTypedGroup<Alphabet>();
+		grpIcons = new FlxTypedGroup<HealthIcon>();
 
-        add(bg);
+		character_path = ${Paths.modsLoc} + "/skins/" + character + "/";
+
+		skinPaths = [];
+
+		var i = 0;
+		addText(i, "Vanilla", 20, 20, false, character);
+		for (file in FileSystem.readDirectory(character_path)) {
+			var path = haxe.io.Path.join([character_path, file]);
+			if (FileSystem.isDirectory(path)) {
+				i++;
+				addText(i, file, grpIcons.members[0].x, grpIcons.members[0].y + (100 * i), true, path + "/icon.png");
+			}
+		}
+		add(grpIcons);
 		add(grpTexts);
 
-        updateMenu();
+		curSelected = 0;
+
+		CoolUtil.resetCameraScroll();
 
 		if (inGame)
 			cameras = [PlayState.camStatic];
-    }
+	}
 
-    override function update(elapsed:Float) {
+	function addText(index:Int, name:String, x:Float, y:Float, isPath:Bool = true, arg:String) {
+		if (name == "Vanilla") {
+			skinPaths.push(null);
+		}
+		else {
+			skinPaths.push(name);
+		}
+
+		var icon = new HealthIcon(null);
+		icon.ID = index;
+		if (isPath) {
+			icon.setCharFromPath(arg);
+		}
+		else {
+			icon.setChar(arg);
+		}
+		icon.setPosition(x, y);
+		icon.setGraphicSize(Std.int(icon.width * 0.8));
+		icon.updateHitbox();
+		grpIcons.add(icon);
+
+		var alphabet = new Alphabet(icon.x + icon.width + 10, icon.y + (icon.height / 4), name, false, false, 0.8);
+		alphabet.ID = index;
+		alphabet.fontColor = FlxColor.WHITE;
+		grpTexts.add(alphabet);
+	}
+
+	override function update(elapsed:Float) {
 		super.update(elapsed);
 
 		if (Controls.check(UI_UP)) {
 			FlxG.sound.play(Paths.sound('scrollMenu'));
-			curSelected -= 1;
+			curSelected--;
 		}
 
 		if (Controls.check(UI_DOWN)) {
 			FlxG.sound.play(Paths.sound('scrollMenu'));
-			curSelected += 1;
+			curSelected++;
 		}
 
-        if (Controls.check(BACK)) {
+		if (Controls.check(BACK)) {
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 			closeSubState();
 			FlxG.state.openSubState(new OptionsPrefencesSubState(inGame));
 		}
 
-        if (curSelected < 0)
-			curSelected = character_list.length - 1;
-
-		if (curSelected >= character_list.length)
-			curSelected = 0;
-
-		grpTexts.forEach(function(txt:FlxText) {
-			if (txt.ID == curSelected)
-				txt.color = FlxColor.YELLOW;
-            else
-                txt.color = FlxColor.WHITE;
-		});
-
-        if (Controls.check(ACCEPT)) {
+		if (Controls.check(ACCEPT)) {
 			FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
-            if (character_list[curSelected] == "Vanilla") {
-                switch (character) {
-                    case "bf":
-                        Options.customBf = false;
-                        if (inGame) PlayState.bf = new Boyfriend(PlayState.bf.x, PlayState.bf.y, PlayState.SONG.player1);
-                    case "gf":
-                        Options.customGf = false;
-                        if (inGame) PlayState.gf = new Character(PlayState.gf.x, PlayState.gf.y, PlayState.gfVersion);
-                    case "dad":
-                        Options.customDad = false;
-                        if (inGame) PlayState.dad = new Character(PlayState.dad.x, PlayState.dad.y, PlayState.SONG.player2);
-                }
-            } else {
-                switch (character) {
-                    case "bf":
-                        Options.customBf = true;
-                        Options.customBfPath = character_path + character_list[curSelected] + "/";
-                        if (inGame) PlayState.bf = new Boyfriend(PlayState.bf.x, PlayState.bf.y, "bf-custom");
-                    case "gf":
-                        Options.customGf = true;
-                        Options.customGfPath = character_path + character_list[curSelected] + "/";
-                        if (inGame) PlayState.gf = new Character(PlayState.gf.x, PlayState.gf.y, "gf-custom");
-                    case "dad":
-                        Options.customDad = true;
-                        Options.customDadPath = character_path + character_list[curSelected] + "/";
-                        if (inGame) PlayState.dad = new Character(PlayState.dad.x, PlayState.dad.y, "dad-custom");
-                }
-            }
-            Options.saveAll();
-            if (inGame) {
-                PlayState.currentPlaystate.updateChar(character);
-            }
-        }
-    }
-
-    function updateMenu() {
-		grpTexts.clear();
-        var i = 0;
-		for (character in character_list) {
-			var optionText:FlxText = new FlxText(20, 20 + (i * 50), 0, character, 32);
-			optionText.scrollFactor.set();
-			optionText.ID = i;
-			optionText.setBorderStyle(FlxTextBorderStyle.OUTLINE, FlxColor.BLACK, 2);
-			grpTexts.add(optionText);
-            i++;
+			if (grpTexts.members[curSelected].text == "Vanilla") {
+				switch (character) {
+					case "bf":
+						Options.customBf = false;
+						if (inGame)
+							PlayState.bf = new Boyfriend(PlayState.bf.x, PlayState.bf.y, PlayState.SONG.player1);
+					case "gf":
+						Options.customGf = false;
+						if (inGame)
+							PlayState.gf = new Character(PlayState.gf.x, PlayState.gf.y, PlayState.gfVersion);
+					case "dad":
+						Options.customDad = false;
+						if (inGame)
+							PlayState.dad = new Character(PlayState.dad.x, PlayState.dad.y, PlayState.SONG.player2);
+				}
+			}
+			else {
+				switch (character) {
+					case "bf":
+						Options.customBf = true;
+						Options.customBfPath = character_path + skinPaths[curSelected] + "/";
+						if (inGame) {
+							PlayState.bf = new Boyfriend(PlayState.bf.x, PlayState.bf.y, "bf-custom", true);
+						}
+					case "gf":
+						Options.customGf = true;
+						Options.customGfPath = character_path + skinPaths[curSelected] + "/";
+						if (inGame) {
+							PlayState.gf = new Character(PlayState.gf.x, PlayState.gf.y, "gf-custom", false, false, true);
+						}
+					case "dad":
+						Options.customDad = true;
+						Options.customDadPath = character_path + skinPaths[curSelected] + "/";
+						if (inGame) {
+							PlayState.dad = new Character(PlayState.dad.x, PlayState.dad.y, "dad-custom", false, false, true);
+						}
+				}
+			}
+			Options.saveAll();
+			if (inGame) {
+				PlayState.currentPlaystate.updateChar(character);
+			}
 		}
 	}
 
-	var curSelected:Int;
+	var curSelected(default, set):Int;
+
+	function set_curSelected(value:Int):Int {
+		if (value < 0)
+			value = grpTexts.length - 1;
+
+		if (value >= grpTexts.length)
+			value = 0;
+
+		grpTexts.forEach(function(txt:Alphabet) {
+			if (txt.ID == value)
+				txt.fontColor = FlxColor.YELLOW;
+			else
+				txt.fontColor = FlxColor.WHITE;
+		});
+		
+		return curSelected = value;
+	}
+
+	var grpTexts:FlxTypedGroup<Alphabet>;
+	var grpIcons:FlxTypedGroup<HealthIcon>;
+
+	var character_path:String;
+
+	var character:String;
 }
 
 class ControlsItem extends FlxTypedSpriteGroup<Alphabet> {
@@ -746,5 +794,168 @@ class OptionsControlsSubstate extends FlxSubState {
 				}
 			}
 		});
+	}
+}
+
+class NoteOption extends FlxSubState {
+	public var notes:FlxSpriteGroup;
+	public var colors:Array<FixedHSLFlxColorShit> = [];
+
+	public function new(inGame:Bool) {
+		super();
+		this.inGame = inGame;
+	}
+	
+	override public function create() {
+		var bg = new Background(FlxColor.MAGENTA);
+		add(bg);
+
+		notes = new FlxSpriteGroup();
+
+		for (i in 0...4) {
+			var babyArrow:FlxSprite = new FlxSprite(0, 0);
+			babyArrow.ID = i + 1;
+			babyArrow.frames = Paths.getSparrowAtlas('NOTE_assets');
+			babyArrow.animation.addByPrefix('green', 'green0');
+			babyArrow.animation.addByPrefix('blue', 'blue0');
+			babyArrow.animation.addByPrefix('purple', 'purple0');
+			babyArrow.animation.addByPrefix('red', 'red0');
+
+			babyArrow.antialiasing = true;
+			babyArrow.setGraphicSize(Std.int(babyArrow.width * Note.sizeShit));
+			
+			switch (i) {
+				case 0:
+					babyArrow.animation.play('purple');
+				case 1:
+					babyArrow.animation.play('blue');
+				case 2:
+					babyArrow.animation.play('green');
+				case 3:
+					babyArrow.animation.play('red');
+			}
+			babyArrow.updateHitbox();
+			babyArrow.x += (Note.getSwagWidth(4) - (4 > 5 ? 2 * (4 - 5) : 0)) * i;
+			colors.push(new FixedHSLFlxColorShit(babyArrow.color.hue, babyArrow.color.saturation, babyArrow.color.brightness));
+			notes.add(babyArrow);
+		}
+		add(nigSquare);
+		add(notes);
+		notes.screenCenter(X);
+		notes.y += 100;
+
+		curSelected = 0;
+
+		notes.members[0].shader = new ColorShader();
+
+		CoolUtil.resetCameraScroll();
+
+		super.create();
+	}
+
+	override public function update(elapsed:Float) {
+		super.update(elapsed);
+
+		if (Controls.check(UI_RIGHT))
+			curSelected++;
+
+		if (Controls.check(UI_LEFT))
+			curSelected--;
+
+		if (Controls.check(BACK)) {
+			Options.saveAll();
+			Options.applyAll();
+			closeSubState();
+			FlxG.state.openSubState(new OptionsPrefencesSubState(inGame));
+		}
+
+		if (FlxG.keys.pressed.Y)
+			notes.members[curSelected].colorTransform.redOffset += 1;
+		if (FlxG.keys.pressed.H)
+			notes.members[curSelected].colorTransform.redOffset -= 1;
+
+		if (FlxG.keys.pressed.U)
+			notes.members[curSelected].colorTransform.greenOffset += 0.01;
+		if (FlxG.keys.pressed.J)
+			notes.members[curSelected].colorTransform.greenOffset -= 0.01;
+
+		if (FlxG.keys.pressed.I)
+			notes.members[curSelected].colorTransform.blueOffset += 0.01;
+		if (FlxG.keys.pressed.K)
+			notes.members[curSelected].colorTransform.blueOffset -= 0.01;
+
+		/*
+		if (FlxG.keys.pressed.O)
+			colors[curSelected].alpha++;
+		if (FlxG.keys.pressed.L)
+			colors[curSelected].alpha--;
+		*/
+	}
+
+	//name begins with funny to rage someone who will read it, i'm not racist
+	var nigSquare:FlxSprite = new FlxSprite();
+
+	var curSelected(default, set):Int;
+
+	function set_curSelected(value:Int):Int {
+		if (value < 0)
+			value = notes.length - 1;
+
+		if (value >= notes.length)
+			value = 0;
+		curSelected = value;
+
+		nigSquare.makeGraphic(Std.int(notes.members[curSelected].width), Std.int(notes.members[curSelected].height), FlxColor.BLACK);
+		nigSquare.alpha = 0.6;
+		nigSquare.setPosition(notes.members[curSelected].x, notes.members[curSelected].y);
+
+		return curSelected;
+	}
+
+	var inGame:Bool;
+}
+
+
+/**
+ * fuck, typo
+ * unused
+ */
+class FixedHSLFlxColorShit {
+	public var hue(default, set):Float = 0;
+	public var saturation(default, set):Float = 0;
+	public var brightness(default, set):Float = 1;
+
+	public function new(hue:Float = 0, saturation:Float = 0, brightness:Float = 1) {
+		this.hue = hue;
+		this.saturation = saturation;
+		this.brightness = brightness;
+	}
+
+	function set_hue(value:Float):Float {
+		if (value > 360)
+			value = 360;
+		if (value < 0)
+			value = 0;
+		return hue = value;
+	}
+
+	function set_saturation(value:Float):Float {
+		if (value > 1)
+			value = 1;
+		if (value < 0)
+			value = 0;
+		return saturation = value;
+	}
+
+	function set_brightness(value:Float):Float {
+		if (value > 1)
+			value = 1;
+		if (value < 0)
+			value = 0;
+		return brightness = value;
+	}
+
+	public function getColor():FlxColor {
+		return FlxColor.fromHSB(hue, saturation, brightness);
 	}
 }
